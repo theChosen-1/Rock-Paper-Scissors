bc9efdef15ac1f4a29016038b0ff5b4ce43a1836
Inshaal <inshaal81@gmail.com>
2025-07-23 22:29:17 +0500

    compeleted playRound function.

--- script.js
+++ script.js
@@ -19,7 +19,42 @@ function getHumanChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
-
+    /* human choice is rock */
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore += 1;
+        return "You Win! rock beats scissors"
+    }
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore += 1;
+        return "You Loose! paper beats rock"
+    }
+    if (humanChoice == "rock" && computerChoice == "rock") {
+        return "Nobody Wins, both put rock!"
+    }
+    /* human choice is paper */
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore += 1;
+        return "You Win! paper beats rock"
+    }
+    if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore += 1;
+        return "You Loose! scissors beats paper"
+    }
+    if (humanChoice == "paper" && computerChoice == "paper") {
+        return "Nobody Wins, both put paper!"
+    }
+    /* human choice is scissors */
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore += 1;
+        return "You Win! scissors beat paper"
+    }
+    if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore += 1;
+        return "You Loose! rock beats scissors"
+    }
+    if (humanChoice == "scissors" && computerChoice == "scissors") {
+        return "Nobody Wins, both put scissors!"
+    }
 }
 
 const humanSelection = getHumanChoice();
