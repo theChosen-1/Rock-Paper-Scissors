83e295fa5218b5b271f0ce7995335bb616802497
Inshaal <inshaal81@gmail.com>
2025-07-23 22:09:16 +0500

    added fucntion to get human choice, global score variables and skeleton for the function to play a round

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let humanScore = 0;
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     if (randomNumber === 1) {
@@ -9,5 +13,16 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let choice = prompt("Please enter your choice: ");
+    return choice.toLowerCase()
+}
 
+function playRound(humanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
